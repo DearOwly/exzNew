699ca585f81c2c59f5d5fa90f059576f7cbc626a
DearOwly <mydearshinchan@gmail.com>
2023-06-08 04:22:52 +0300

    added variants

--- src/main/java/org/example/tasks/Tasks.java
+++ src/main/java/org/example/tasks/Tasks.java
@@ -18,36 +18,61 @@ public class Tasks {
         this.parser = parser;
     }
 
-    public List<Entity> task1(){
+    public List<Entity> task0(){
         return parser.parseInfo();
     }
 
-    public void task2(){
-        double averageOfWeight = task1()
-                .stream()
-                .filter(entity -> entity.getBwt() != null)
-                .mapToInt(e -> Integer.parseInt(e.getBwt()))
-                .average().orElseThrow(() -> new RuntimeException("ou"));
+    public void task1Var1(){
+
     }
 
-    public boolean task3(){
-        double averageOfSmokers = task1()
+    //помогите сделать это по расам....asian black hispanic other
+    public double helper(String race){
+        double averageOfWeight = task0()
                 .stream()
-                .filter(entity -> entity.getGender().equals("F")
-                        && entity.getSmoker().equals("Y"))
+                .filter(entity -> entity.getRace().equals(race))
                 .mapToInt(e -> Integer.parseInt(e.getBwt()))
                 .average().orElseThrow(() -> new RuntimeException("ou"));
-        double averageOfNonSmoker = task1()
+        return averageOfWeight;
+    }
+    public void task1Var2(){
+        double averageOfWeightWhite = helper("white");
+        double averageOfWeightBlack = helper("black");
+        double averageOfWeightHispanic = helper("hispanic");
+        double averageOfWeightAsian = helper("asian");
+        double averageOfWeightOther = helper("other");
+    }
+
+    public boolean task2Var1(){
+        double avarageOfMarried = task0()
                 .stream()
-                .filter(entity -> entity.getGender().equals("F")
-                        && entity.getSmoker().equals("N"))
-                .mapToInt(e -> Integer.parseInt(e.getBwt()))
+                .filter(entity -> entity.getMarried().equals("1"))
+                .mapToInt(e -> Integer.parseInt(e.getGestation()))
+                .average().orElseThrow(() -> new RuntimeException("ou"));
+        double avarageOfNonMarried = task0()
+                .stream()
+                .filter(entity -> entity.getMarried().equals("0"))
+                .mapToInt(e -> Integer.parseInt(e.getGestation()))
+                .average().orElseThrow(() -> new RuntimeException("ou"));
+        return avarageOfMarried > avarageOfNonMarried;
+    }
+
+    public boolean task2Var2(){
+        double averageOfSmokers = task0()
+                .stream()
+                .filter(entity -> entity.getSmoker().equals("Y"))
+                .mapToInt(e -> Integer.parseInt(e.getGestation()))
+                .average().orElseThrow(() -> new RuntimeException("ou"));
+        double averageOfNonSmoker = task0()
+                .stream()
+                .filter(entity -> entity.getSmoker().equals("N"))
+                .mapToInt(e -> Integer.parseInt(e.getGestation()))
                 .average().orElseThrow(() -> new RuntimeException("ou"));
         return averageOfNonSmoker > averageOfSmokers;
     }
 
-    public void task4(){
-        task1().stream().map(entity -> ResultEntity.builder()
+    public void task3Var2(){
+        task0().stream().map(entity -> ResultEntity.builder()
                 .educationTime(entity.getEducation())
                 .motherRace(entity.getRace())
                 .build().toString()/*.getBytes(StandardCharsets.UTF_8)*/)
